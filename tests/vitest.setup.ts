a617e7c6376b28c9a1d9cf7d79e706ba29763d5b
lschafer <lschafer@ethz.ch>
2025-09-03 16:09:33 +0200

    add vitest config

---
+++ tests/vitest.setup.ts
@@ -0,0 +1,20 @@
+import { config } from 'dotenv'
+config({ path: '.env.test' })
+
+import { resetTestDatabase } from './utils/test-db'
+import {beforeEach} from "vitest";
+
+beforeEach(async () => {
+    await resetTestDatabase({ seed: true })
+})
+
+import { readBody, createError, defineEventHandler, eventHandler } from 'h3'
+// make them available like Nitro does
+// @ts-ignore
+globalThis.defineEventHandler = defineEventHandler
+// @ts-ignore
+globalThis.eventHandler = eventHandler
+// @ts-ignore
+globalThis.readBody = readBody
+// @ts-ignore
+globalThis.createError = createError
---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import path from 'node:path'
+
+export default defineConfig({
+    test: {
+        environment: 'node',
+        globals: true,
+        setupFiles: ['./tests/vitest.setup.ts'],
+        include: ['tests/**/*.spec.ts'],
+    },
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, '.'),  // so ~/server/utils/prisma works
+            '#': path.resolve(__dirname, '.'),  // optional, Nuxt also uses #
+            '@': path.resolve(__dirname, '.'),  // optional convenience
+        },
+    },
+})
