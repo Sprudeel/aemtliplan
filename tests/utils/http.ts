9e58d3514873b1748068658a492ac16ed436ef2b
lschafer <lschafer@ethz.ch>
2025-09-03 16:10:21 +0200

    add test utils

---
+++ tests/utils/http.ts
@@ -0,0 +1,19 @@
+import { createApp, eventHandler, toNodeListener } from 'h3'
+import supertest from 'supertest'
+
+import groupPost from '../../server/api/groups/group.post'
+import groupsGet from '../../server/api/groups/groups.get'
+import setupGet from '../../server/api/setup.get' // if you have it
+
+export function createTestAgent() {
+    const app = createApp()
+
+
+    app.use('/api/groups', eventHandler(groupsGet))
+    app.use('/api/group', eventHandler(groupPost))
+    app.use('/api/setup', eventHandler(setupGet))
+
+
+    const listener = toNodeListener(app)
+    return supertest(listener)
+}
