a3429db4788d44c96bcc79c0c5cf0779f2417365
lschafer <lschafer@ethz.ch>
2025-10-10 15:26:51 +0200

    lint

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,7 +1,6 @@
 import { config } from "dotenv";
 config({ path: ".env.test" });
 
-
 import { readBody, createError, defineEventHandler, eventHandler } from "h3";
 // make them available like Nitro does
 // @ts-ignore
