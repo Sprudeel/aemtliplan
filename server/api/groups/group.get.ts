b81e793b30e59d206ee39fe56921be38f52e7b73
lschafer <lschafer@ethz.ch>
2025-10-10 15:27:49 +0200

    lint and secure endpoints

--- server/api/groups/group.get.ts
+++ server/api/groups/group.get.ts
@@ -1,20 +1,20 @@
 import prisma from "~/server/utils/prisma";
-import {z} from "zod";
-import { defineEventHandler, getQuery, createError } from 'h3'
+import { z } from "zod";
+import { defineEventHandler, getQuery, createError } from "h3";
 
 const Query = z.object({
-    id: z.coerce.number().int(),
+  id: z.coerce.number().int(),
 });
 
 export default defineEventHandler(async (event) => {
-    const { id } = Query.parse(await getQuery(event));
-    const group = await prisma.group.findUnique({
-            where: { id },
-            include: { members: true },
-    });
+  const { id } = Query.parse(await getQuery(event));
+  const group = await prisma.group.findUnique({
+    where: { id },
+    include: { members: true },
+  });
 
-    if (!group) {
-        throw createError({ statusCode: 404, statusMessage: "Group not found" });
-    }
-    return group;
+  if (!group) {
+    throw createError({ statusCode: 404, statusMessage: "Group not found" });
+  }
+  return group;
 });
